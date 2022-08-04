d0890b776bce3fcb06d2c76172471c369bb95fcf
Im.GW <50003828+lim12749@users.noreply.github.com>
2022-08-04 11:22:57 +0900

    update

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -4,32 +4,44 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    public LineRenderer bulletLine;
-    public Transform gunFirePosition;
+    public AudioSource audioSource; //소리를 관리하는 기능
+    public AudioClip gunFireSound; //소리를 가진 칩
+
+    public LineRenderer bulletLine; //화면에 선을 그리는 기능
+    public Transform gunFirePosition; //위치를 관리하는 기능
     public float distance;
+
+    public float timenow;
+    public float lastFireTime;// 총을 마지막으로 발사한 시점
+    public float fireDelayTime =0.12f; //총을 발사한 지연시간 (연사력)
 
     private void Start()
     {
+        //소리를 관리하는 기능을 가져옴
+        audioSource = GetComponent<AudioSource>();
         bulletLine = GetComponent<LineRenderer>();
     }
     private void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        timenow = Time.time;
+        //마우스 클릭
+        if (Input.GetButton("Fire1"))
         {
-            Debug.Log("왼쪽 클릭");
-            RaycastHit hit;
 
-            if(Physics.Raycast(gunFirePosition.position,
-                gunFirePosition.forward*distance, out hit))
+            if (Time.time >= lastFireTime + fireDelayTime)
             {
-                Debug.Log(hit.collider.name);
-               
-            }
-            Debug.DrawRay(gunFirePosition.position, 
-                gunFirePosition.forward * distance, 
-                Color.red);
+                RaycastHit hit;
 
-            StartCoroutine(ShotEffect(hit.point));
+                if (Physics.Raycast(gunFirePosition.position, gunFirePosition.forward * distance, out hit))
+                {
+                    Debug.Log(hit.collider.name);
+                }
+                Debug.DrawRay(gunFirePosition.position, gunFirePosition.forward * distance,
+                    Color.red);
+
+                StartCoroutine(ShotEffect(hit.point));
+                lastFireTime = Time.time;
+            }
         }
     }
 
@@ -39,6 +51,7 @@ public class Gun : MonoBehaviour
         bulletLine.enabled = true;
         bulletLine.SetPosition(0, gunFirePosition.position);
         bulletLine.SetPosition(1, hitPotion);
+        audioSource.PlayOneShot(gunFireSound);
         yield return new WaitForSeconds(0.03f);
 
         bulletLine.enabled = false;
